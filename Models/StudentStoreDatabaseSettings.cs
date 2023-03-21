46c3f765fd713da70ec17ae7773caad7e2df4158
Munteanu Silviu <munteanusilviu1996@yahoo.com>
2023-03-21 17:17:17 +0200

    First commit for this project

---
+++ Models/StudentStoreDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿
+namespace StudentManagement.Models
+{
+    public class StudentStoreDatabaseSettings : IStudentStoreDatabaseSettings
+    {
+        string StudentCoursesCollectionName { get; set; } = String.Empty;
+        string IStudentStoreDatabaseSettings.StudentCoursesCollectionName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string ConnectionString { get; set; } = String.Empty;
+        string IStudentStoreDatabaseSettings.ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string DatabaseName { get; set; } = String.Empty;
+        string IStudentStoreDatabaseSettings.DatabaseName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
